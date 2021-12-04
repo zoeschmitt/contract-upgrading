79214cd8b6d58da17144244d280e6a5d26ef8ad4
Zoe Schmitt <zoeschmitt@hotmail.com>
2021-12-03 22:21:44 -0600

    proxy admin added

---
+++ scripts/deploy_and_upgrade.py
@@ -0,0 +1,31 @@
+from brownie import network, Box, ProxyAdmin, TransparentUpgradeableProxy, Contract, BoxV2
+from scripts.helpful_scipts import encode_function_data, get_account
+
+
+def main():
+    account = get_account()
+    print(f"Deploying to {network.show_active()}")
+    # implementation
+    box = Box.deploy({"from": account})
+
+    # proxy admin
+    proxy_admin = ProxyAdmin.deploy({"from": account})
+    # initializer = box.store, 1
+    box_encoded_initializer_func = encode_function_data()
+    proxy = TransparentUpgradeableProxy.deploy(
+        box.address,
+        proxy_admin.address,
+        box_encoded_initializer_func,
+        {"from": account, "gas_limit": 1000000},  # gas limit optional
+    )
+    print(f"Proxy deplpoyed to {proxy}, you can now upgrade to v2!")
+    # assigning proxy address the abi of the box contract. Works
+    # bc the proxy will delegate all its calls to box
+    proxy_box = Contract.from_abi("Box", proxy.address, Box.abi)
+    proxy_box.store(1, {"from": account})
+    # print(proxy_box.retrieve())
+
+    # upgrade to v2
+    box_v2 = BoxV2.deploy({"from": account})
+    upgrade()
+    
